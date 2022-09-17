cd0e7804c4395a925435af068f87caf15852e58d
Karolinny Guimaraes <karolinny.silva@gmail.com>
2022-09-16 22:38:19 -0300

    environment variables

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from os import getenv
+from dotenv import load_dotenv
+load_dotenv()
+
+APP_PORT = int(getenv('APP_PORT'))
+DEBUG = eval(getenv('DEBUG').title())
 
 app = Flask(__name__)
 
 if __name__ == '__main__':
     host = '0.0.0.0'
-    app.run(host=host, port=5000, debug=True)
+    debug = DEBUG
+    port = APP_PORT
+    app.run(host=host, port=port, debug=debug)
